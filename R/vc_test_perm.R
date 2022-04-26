53415057788940bb0b2c2e43397067ddff976b52
borishejblum <boris.hejblum@u-bordeaux.fr>
2022-04-26 20:49:59 +0200

    perm fixed (it wasn't a bug!)

--- R/vc_test_perm.R
+++ R/vc_test_perm.R
@@ -185,7 +185,7 @@ vc_test_perm <- function(y, x, indiv = rep(1, nrow(x)), phi, w,
             nperm_sup_obs_threshold <- rowSums(gene_scores_perm_threshold >= 
                                                  gene_scores_obs[ind_threshold])
             pvals_e_threshold <- permPvals(nPermSupObs = nperm_sup_obs_threshold, 
-                                           nPermEff = n_perm_threshold,
+                                           nPermEff = 2*n_perm_threshold,
                                            totalPossibleNPerm = N_possible_perms)
             
             pvals_e[ind_threshold] <- pvals_e_threshold
