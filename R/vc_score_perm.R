08b62878058f0c1931399c3bfb1b52aaa73d3d2a
Larrouture Iban <iban.larrouture@u-bordeaux.fr>
2023-07-31 14:43:25 +0200

    correct the assignment = into the proper <-

--- R/vc_score_perm.R
+++ R/vc_score_perm.R
@@ -242,7 +242,7 @@ vc_score_perm <- function(y, x, indiv, phi, w, Sigma_xi = diag(ncol(phi)),
         pboptions(opb)
     }
     if(is.vector(gene_Q)){
-      gene_Q = matrix(gene_Q, nrow = 1)
+      gene_Q <- matrix(gene_Q, nrow = 1)
     }
 
     rownames(gene_Q) <- colnames(yt_mu)
